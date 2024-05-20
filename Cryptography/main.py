d30edfccfecb185472609394e6f4d770fda0553e
abhiraj-mengade <abhiraj.mengade@outlook.com>
2024-05-20 19:53:56 +0530

    Add Xor functions

---
+++ Cryptography/main.py
@@ -0,0 +1,38 @@
+import string
+import collections
+
+
+def is_space(char):
+    return char == 0x00 or chr(char) in string.ascii_letters
+
+
+def xor(a, b):
+    return [c ^ d for c, d in zip(a, b)]
+
+
+def track_spaces(text) -> collections.Counter:
+    counter: collections.Counter = collections.Counter()
+    for index, char in enumerate(text):
+        if is_space(char):
+            counter[index] += 1
+
+    return counter
+
+
+def recover_partial_key(ciphertexts):
+    shortest_text = min(len(text) for text in ciphertexts)
+    key = [None for _ in range(shortest_text)]
+
+    for main_index, main_ciphertext in enumerate(ciphertexts):
+        main_counter: collections.Counter = collections.Counter()
+
+        for secondary_index, secondary_ciphertext in enumerate(ciphertexts):
+            if main_index != secondary_index:
+                main_counter.update(
+                    track_spaces(xor(main_ciphertext, secondary_ciphertext)))
+
+        for index, count in main_counter.items():
+            if count == len(ciphertexts) - 1:
+                key[index] = ord(' ') ^ main_ciphertext[index]
+
+    return key
